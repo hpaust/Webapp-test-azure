7e015ceac1a3ddf606b265a40ce086e4907e4142
Henrik Sogn Paust <henrik.paust@idmngmnt.no>
2018-10-31 11:12:29 +0100

    jwt-try added

--- application.py
+++ application.py
@@ -29,17 +29,85 @@
 #if __name__ == '__main__':
 #  app.run()
 
-from flask import Flask
+#from flask import Flask
+#from flask_restful import Resource, Api
+ 
+#app = Flask(__name__)
+#api = Api(app)
+ 
+#class HelloWorld(Resource):
+#    def get(self):
+#        return {'hello': 'world'}
+ 
+#api.add_resource(HelloWorld, '/')
+ 
+#if __name__ == '__main__':
+#    app.run(debug=True)
+
+from flask import Flask,jsonify
 from flask_restful import Resource, Api
- 
+from flask_jwt import JWT, jwt_required
+from flask import make_response
+
+import pandas as pd
+
+
+
 app = Flask(__name__)
-api = Api(app)
- 
-class HelloWorld(Resource):
+app.config['SECRET_KEY'] = 'super-secret'
+
+api = Api(app, prefix="/api/v1")
+
+USER_DATA = {
+    "masnun": "abc123"
+}
+
+
+class User(object):
+    def __init__(self, id):
+        self.id = id
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
+
+
+def verify(username, password):
+    if not (username and password):
+        return False
+    if USER_DATA.get(username) == password:
+        return User(id=123)
+
+
+def identity(payload):
+    user_id = payload['identity']
+    return {"user_id": user_id}
+
+
+jwt = JWT(app, verify, identity)
+
+
+class PrivateResource(Resource):
+    @jwt_required()
     def get(self):
-        return {'hello': 'world'}
- 
-api.add_resource(HelloWorld, '/')
- 
+        return {"meaning_of_life": 42}
+
+
+api.add_resource(PrivateResource, '/private')
+
+class GetDagensvitsFromExcel(Resource):
+    @jwt_required()
+    def get(self):
+        df = pd.read_excel('testFlaskApi.xlsx')
+        a = df.loc[0,'brukernavn']
+        b=df.loc[0,'passord']
+        return {"dagens":a,"vits":b}
+
+api.add_resource(GetDagensvitsFromExcel,'/vits')
+        
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 if __name__ == '__main__':
     app.run(debug=True)
